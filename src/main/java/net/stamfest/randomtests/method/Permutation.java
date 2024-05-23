84c9b21926637f5351296439ce2c7fd987f32619
Vlado <vladzhu607@gmail.com>
2024-05-23 08:13:34 +0300

    perm

--- src/main/java/net/stamfest/randomtests/method/Permutation.java
+++ src/main/java/net/stamfest/randomtests/method/Permutation.java
@@ -10,15 +10,38 @@ import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class Permutation {
 
-    private class S {
+    private static class S {
+        int compRes;         // 1
+        int runsRes1;        // 1
+        int runsRes2;        // 1
+        double derRes;       // 1
+        int runsTrendRes1;   // 1
+        int runsTrendRes2;   // 1
+        int runsTrendRes3;   // 1
+        double covRes;       // 1
+        int colisRes1;       // 1
+        int colisRes2;       // 1
 
+        //                     10
+
+
+        public S(int compRes, int runsRes1, int runsRes2, double derRes, int runsTrendRes1, int runsTrendRes2,
+                 int runsTrendRes3, double covRes, int colisRes1, int colisRes2) {
+            this.compRes = compRes;
+            this.runsRes1 = runsRes1;
+            this.runsRes2 = runsRes2;
+            this.derRes = derRes;
+            this.runsTrendRes1 = runsTrendRes1;
+            this.runsTrendRes2 = runsTrendRes2;
+            this.runsTrendRes3 = runsTrendRes3;
+            this.covRes = covRes;
+            this.colisRes1 = colisRes1;
+            this.colisRes2 = colisRes2;
+        }
     }
 
 
@@ -33,29 +56,74 @@ public class Permutation {
 
     public static void main(String[] args) throws IOException {
         Bits bits = IO.readAscii(Objects.requireNonNull(
-                Files.newInputStream(Path.of("data.e"))), 1000000);
+                Files.newInputStream(Path.of("data.e"))), 10000);
 
         ArrayList<Integer> integers = new ArrayList<>();
         bits.forEach(integers::add);
         long N = integers.size();
+
+        S[][] Sij= new S[10][1001];
+
+        int compRes;
+        int[] runsRes;
+        double derRes;
+        int[] runsTrendRes;
+        double covRes;
+        int[] colisRes;
+        S temp;
 
         for (int i = 0; i < 10; i++) {
             ArrayList<Integer> newSeq = new ArrayList<>();
             for (int j = (int) (i * N / 10); j < (i + 1) * N / 10; j++) {
                 newSeq.add(integers.get(j));
             }
-            int compRes = compressionTest(newSeq);
-            int[] runsRes = runsTest(newSeq);
-            double derRes = maxCumDev(newSeq);
-            int[] runsTrendRes = runsTestWithTrend(newSeq);
-            double covRes = covarianceTest(newSeq);
-            int[] colisRes = collisionsTest(newSeq);
-            System.out.println(compRes + " - " + runsRes[0] + ", " + runsRes[1] + " - " + derRes + " - " +
-                    runsTrendRes[0] + ", " + runsTrendRes[1] + ", " + runsTrendRes[2] + " - " + covRes +
-                    "  " + colisRes[0] + ", " + colisRes[1]);
+            compRes = compressionTest(newSeq);
+            runsRes = runsTest(newSeq);
+            derRes = maxCumDev(newSeq);
+            runsTrendRes = runsTestWithTrend(newSeq);
+            covRes = covarianceTest(newSeq);
+            colisRes = collisionsTest(newSeq);
+
+            temp = new S(compRes, runsRes[0], runsRes[1], derRes, runsTrendRes[0], runsTrendRes[1], runsTrendRes[2],
+                    covRes, colisRes[0], colisRes[1]);
+
+            Sij[i][0] = temp;
+
+            for(int j = 1; j <= 1000; j++) {
+                fisherYates(newSeq);
+                compRes = compressionTest(newSeq);
+                runsRes = runsTest(newSeq);
+                derRes = maxCumDev(newSeq);
+                runsTrendRes = runsTestWithTrend(newSeq);
+                covRes = covarianceTest(newSeq);
+                colisRes = collisionsTest(newSeq);
+
+                temp = new S(compRes, runsRes[0], runsRes[1], derRes, runsTrendRes[0], runsTrendRes[1], runsTrendRes[2],
+                        covRes, colisRes[0], colisRes[1]);
+                Sij[i][j] = temp;
+            }
+        }
+
+        int[][] R = new int[10][10];
+
+        for(int i = 0; i < 10; i++) {
+
         }
 
 
+
+    }
+
+    public static void fisherYates(ArrayList<Integer> array) {
+        Random rand = new Random();
+
+        for (int i = array.size() - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+
+            int temp = array.get(i);
+            array.set(i, array.get(j));
+            array.set(j, temp);
+        }
     }
 
     public static int compressionTest(ArrayList<Integer> integers) {
